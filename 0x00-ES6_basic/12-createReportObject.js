d4e420400aaf48b70c8d60079860ecdcc4df4e6d
Naod Taddese <naodtds@gmail.com>
2024-06-08 15:43:38 +0300

    task 12 eslint fix

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,13 +1,12 @@
 export default function createReportObject(employeesList) {
   const getNumberOfDepartments = () => Object.keys(employeesList).length;
-  
+
   const report = {
     allEmployees: {
-      ...employeesList
+      ...employeesList,
     },
-    getNumberOfDepartments
+    getNumberOfDepartments,
   };
 
   return report;
 }
-
