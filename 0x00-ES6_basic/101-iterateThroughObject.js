61c618e698c1166f3897d80b11630b0cd794078b
Naod Taddese <naodtds@gmail.com>
2024-06-08 15:47:20 +0300

    task 14

--- 0x00-ES6_basic/101-iterateThroughObject.js
+++ 0x00-ES6_basic/101-iterateThroughObject.js
@@ -1,8 +1,8 @@
 export default function iterateThroughObject(reportWithIterator) {
-    let result = '';
-    for (const emp of reportWithIterator) {
-        result += `${emp} | `;
-    }
+  let result = '';
+  for (const emp of reportWithIterator) {
+    result += `${emp} | `;
+  }
 
-    return result.slice(0, -3);
+  return result.slice(0, -3);
 }
