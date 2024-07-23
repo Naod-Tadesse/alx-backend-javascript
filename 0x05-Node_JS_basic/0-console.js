aa47844a5cc5673a3e1fca79d63619348608e39e
Naod Taddese <naodtds@gmail.com>
2024-07-23 20:45:31 +0300

    finish

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,5 @@
+exports.displayMessage = msg => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,17 @@
+const http = require('http');
+const PORT = 1245;
+const HOST = 'localhost';
+const app = http.createServer();
+
+app.on('request', (req, res) => {
+  const responseText = 'Hello Holberton School!';
+
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', responseText.length);
+  res.statusCode = 200;
+  res.write(Buffer.from(responseText));
+});
+
+app.listen(PORT, HOST, () => {});
+
+module.exports = app;
